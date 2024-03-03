ae06bafb922fb4dcf0b6e9f55b7a5b1dcfd4c09e
priyacse21 <107832742+priyacse21@users.noreply.github.com>
2024-03-03 10:29:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed= new typed("text", {
+    String: ["Frontend Developer" , "Youtuber" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+
